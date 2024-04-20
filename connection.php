b1b85f30053f78f3ec35744bc20eacb538a0b135
Ashrafu <ashraftuyubahe001@gmail.com>
2024-04-20 19:20:51 +0200

    finished registering users

--- connection.php
+++ connection.php
@@ -1,15 +1,12 @@
 <?php
-$host= 'locahost';
+$host= 'localhost';
 $usernm='root';
 $pass= 123;
-$dbnm='student';
+$dbnm='learn';
 $conn= new mysqli($host,$usernm,$pass,$dbnm);
 if($conn->connect_error){
     exit("connection failed:".$conn->connect_error);
-} else{
-    echo '<script>alert("invalid credentials")</script>';
-}
-
+} 
 
 
 
